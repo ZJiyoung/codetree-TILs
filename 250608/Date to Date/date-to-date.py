efcd78afae863d3b87433559fb4c6f981fba33de
Jiyoung Kim <kimdanchuchu@gmail.com>
2025-06-07 15:14:18 -0700

    Title: Date to Date, Time: 70ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 250608/Date to Date/date-to-date.py
+++ 250608/Date to Date/date-to-date.py
@@ -9,15 +9,15 @@ day = d1
 elapsed_days = 1
 
 while True:
-    if month == m2 and day == d2:
-        elapsed_days += 1
-        break
-    
     elapsed_days += 1
     day += 1 
 
     if day == days[month]:
         month += 1
         day = 1
+    
+    if month == m2 and day == d2:
+        elapsed_days += 1
+        break
     
 print(elapsed_days)
