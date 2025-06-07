6a662a65b87e03cfe79979f84a5ad7691b1dc105
Jiyoung Kim <kimdanchuchu@gmail.com>
2025-06-07 14:44:34 -0700

    Title: Date to Date, Time: 91ms, Memory: 19MB, Status: Runtime Error - Codetree

---
+++ 250608/Date to Date/date-to-date.py
@@ -0,0 +1,21 @@
+m1, d1, m2, d2 = map(int, input().split())
+
+# Please write your code here.
+days = [0,31,28,31,30,31,30,31,31,30,31,30,31]
+#          1. 2. 3. 4. 5  6. 7. 8. 9.10.11.12
+
+month = m1
+day = d1
+
+while True:
+    if month == m2 and day == d2:
+        break
+    
+    elapsed_days += 1
+    day += 1 
+
+    if day == days[month]:
+        month += 1
+        day = 1
+    
+print(elapsed_days)
