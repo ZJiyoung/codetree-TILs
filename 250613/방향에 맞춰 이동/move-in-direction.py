8981ffd603a035159870677d218b75938959c9a5
Jiyoung Kim <kimdanchuchu@gmail.com>
2025-06-13 00:47:10 -0700

    Title: 방향에 맞춰 이동, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 250613/방향에 맞춰 이동/move-in-direction.py
+++ 250613/방향에 맞춰 이동/move-in-direction.py
@@ -5,17 +5,20 @@ dist = [int(move[1]) for move in moves]
 
 # Please write your code here.
 
-x = 0
-y = 0
+x, y = 0, 0
+dx, dy = [1, 0, -1, 0], [0, -1, 0, 1]
 
-for i in range(n):
-    if dir[i] == "W":
-        x -= dist[i]
-    elif dir[i] == "N":
-        y += dist[i]
-    elif dir[i] == "S":
-        y -= dist[i]
-    else:
-        x += dist[i]
+for i in range(len(dir)):
+    for _ in range(dist[i]):
+        if dir[i] == 'E':
+            nx, ny = x + dx[0], y + dy[0]
+        elif dir[i] == 'S':
+            nx, ny = x + dx[1], y + dy[1]
+        elif dir[i] == 'W':
+            nx, ny = x + dx[2], y + dy[2]
+        elif dir[i] == 'N':
+            nx, ny = x + dx[3], y + dy[3]
+
+        x, y = nx, ny
 
 print(x, y)
