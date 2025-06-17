2e31f74d15a7a3d48cbf87f526c587f5c374675f
Jiyoung Kim <kimdanchuchu@gmail.com>
2025-06-16 22:06:42 -0700

    Title: G or H 3, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250617/G or H 3/G-or-H-3.py
@@ -0,0 +1,29 @@
+n, k = map(int, input().split())
+x = []
+c = []
+for _ in range(n):
+    pos, char = input().split()
+    x.append(int(pos))
+    c.append(char)
+
+# Please write your code here.
+score = 0 
+max_score = 0
+
+for i in range(n):
+    for j in range(i,n):
+        if (x[j]-x[i]) <= k:
+
+            score = 0
+
+            for t in range(i, j+1):
+                if c[t] == 'G':
+                    score += 1
+                elif c[t] == 'H':
+                    score += 2
+
+            if score > max_score:
+                max_score = score
+
+print(max_score)
+
