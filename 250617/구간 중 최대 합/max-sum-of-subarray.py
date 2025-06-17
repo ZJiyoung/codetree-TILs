18edff65400a502c32ed4e22827fc6b6d55cc570
Jiyoung Kim <kimdanchuchu@gmail.com>
2025-06-16 21:16:16 -0700

    Title: 구간 중 최대 합, Time: 52ms, Memory: 16MB, Status: Passed - Codetree

--- 250617/구간 중 최대 합/max-sum-of-subarray.py
+++ 250617/구간 중 최대 합/max-sum-of-subarray.py
@@ -2,14 +2,21 @@ n, k = map(int, input().split())
 arr = list(map(int, input().split()))
 
 # Please write your code here.
+'''
+6 3
+1 1 1 9 9 9
+
+정답 27 
+나 19
+'''
 
 ksum = 0
 kmax = 0
 
-for i in range(n-k): 
+for i in range(n-k + 1): # 6 - 3 + 1
     ksum = 0
 
-    for j in range(k):
+    for j in range(k): # 0 1 2 
         ksum += arr[i + j]
 
     if ksum > kmax : 
