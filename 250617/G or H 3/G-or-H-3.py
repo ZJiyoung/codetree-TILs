7bb1f02d819e1863a0b9d42516a99cafeb7686b6
Jiyoung Kim <kimdanchuchu@gmail.com>
2025-06-16 22:11:34 -0700

    Title: G or H 3, Time: 60ms, Memory: 17MB, Status: Passed - Codetree

--- 250617/G or H 3/G-or-H-3.py
+++ 250617/G or H 3/G-or-H-3.py
@@ -7,23 +7,21 @@ for _ in range(n):
     c.append(char)
 
 # Please write your code here.
+people = sorted([(x[i], c[i]) for i in range(n)])
 score = 0 
 max_score = 0
 
 for i in range(n):
-    for j in range(i,n):
-        if (x[j]-x[i]) == k:
-
-            score = 0
-
-            for t in range(i, j+1):
-                if c[t] == 'G':
-                    score += 1
-                elif c[t] == 'H':
-                    score += 2
-
-            if score > max_score:
-                max_score = score
+    score = 0
+    for j in range(i, n):
+        if people[j][0] - people[i][0] <= k:
+            if people[j][1] == 'G':
+                score += 1
+            elif people[j][1] == 'H':
+                score += 2
+        else:
+            break
+    max_score = max(max_score, score)
 
 print(max_score)
 
